f4ec14b25a670e7b51c7a17ecf9f8710a31f8d16
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2023-06-06 09:12:35 +0200

    feat:add Searchbar and modules (this is it what do trouble last time

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,57 @@
+import { Component } from 'react';
+import { toast } from 'react-hot-toast';
+import { MdImageSearch } from 'react-icons/md';
+import css from './Searchbar.module.css';
+// import 'react-notifications-component/dist/theme.css';
+
+export class Searchbar extends Component {
+  state = {
+    search: '',
+  };
+
+  onChangeInput = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+
+    console.info('Wpisano wartość:', value);
+  };
+
+  resetForm = () => {
+    this.setState({ search: '' });
+  };
+
+  render() {
+    return (
+      <header className={css.SearchBar}>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+
+            if (!this.state.search) {
+              return toast.error('Enter text for search.');
+            }
+
+            this.props.handleSubmit(this.state.search);
+            this.resetForm();
+          }}
+          className={css.Form}
+        >
+          <button type="submit" className={css.Button}>
+            <MdImageSearch size="30" />
+          </button>
+
+          <input
+            value={this.state.search}
+            onChange={this.onChangeInput}
+            className={css.Input}
+            autoFocus
+            name="search"
+            type="text"
+            autoComplete="off"
+            placeholder="Search images or photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
